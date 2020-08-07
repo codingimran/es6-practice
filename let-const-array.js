01bcfdc2b8b43951fd505a26f1a011c3f8b0736a
Imran Khan <coadingbird@gmail.com>
2020-08-07 11:18:44 +0600

    class 1 finished

---
+++ let-const-array.js
@@ -0,0 +1,22 @@
+const hubby = "Elias Kanchon";
+// hubby = "Omor sunny";
+// console.log(hubby);
+
+const numbers = [12, 45, 12, 44, 55, 33, 54, 45, 23, 26, 3456, 4];
+numbers.push(44333);
+const num = numbers.slice(2, -6);
+const nums = numbers.slice(3, numbers.length);
+// console.log(num);
+// console.log(nums);
+
+const nayok = { name: "Imran Khan", phone: 45454545 };
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+  sum = sum + i;
+  console.log(sum);
+}
